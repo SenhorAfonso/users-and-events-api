f9287e7b9a6e0e40c14e812a22a2d89fe11b4576
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-26 18:33:13 -0300

    :sparkles: feat: express server configuration

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+class App {
+  public server: express.Express;
+
+  constructor() {
+    this.server = express();
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+}
+
+export default new App().server

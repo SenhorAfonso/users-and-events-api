1c77d3e9f2323ea071ff3711cc7535047838f987
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-26 20:10:59 -0300

    :recycle: refactor: database connect function turned into a class

--- src/infra/database/mongo/connectDB.ts
+++ src/infra/database/mongo/connectDB.ts
@@ -1,14 +1,25 @@
 import mongoose from "mongoose"
 
-const connectDB = () => {
-  const mongoURI: string = 'mongodb://127.0.0.1:27017/customers'
-  mongoose.connect(mongoURI)
-    .then(() => {
-      console.log('Database connected!')
-    })
-    .catch((error) => {
-      throw new Error(`Error (${error}) during database connection!`);
-    })
+class DataBase {
+  private ORM: any;
+  private URL: string;
+
+  constructor(ORM: any, URL: string) {
+    this.ORM = ORM;
+    this.URL = URL;
+  }
+
+  connect() {
+    this.ORM.connect(this.URL)
+      .then(() => {
+        console.log('Database connected!')
+      })
+      .catch((error) => {
+        throw new Error(`Error (${error}) during database connection!`);
+      })
+  }
+
 }
 
-connectDB()
+const db = new DataBase(mongoose, 'mongodb://127.0.0.1:27017/events');
+db.connect();

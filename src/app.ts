70e58e208addca012d784a1ab3b1033f47678c34
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-26 19:08:19 -0300

    :sparkles: API connected to database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import server from "./server";
+import connectDB from "./infra/database/mongo/connectDB";
+
 const port: number = 3000;
 
-const start = (): void => {
+const start = async (): Promise<void> => {
   try {
+    await connectDB();
     server.listen(port, () => {
       console.log(`Server is listening at ${port} port.`)
     })

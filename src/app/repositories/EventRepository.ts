c172d9441af14fe1d9a97d97b36ce39d4bda461b
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-27 17:59:30 -0300

    :sparkles: feat: added repository layer for event flow

---
+++ src/app/repositories/EventRepository.ts
@@ -0,0 +1,29 @@
+
+class EventRepository {
+
+  create(payload: any) {
+    const result = 'Create an event';
+    return result;
+  }
+
+  getAll() {
+    const result = 'get an event';
+    return result;
+  }
+
+  getSingle(payload: any) {
+    const result = 'get single event';
+    return result;
+  }
+
+  deleteBy(payload: any) {
+    const result = 'delete an event';
+    return result;
+  }
+
+  deleteSingle(payload: any) {
+    const result = 'delete single event';
+    return result;
+  }
+
+}

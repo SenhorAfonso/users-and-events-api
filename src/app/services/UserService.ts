6f2eb05bbf70635a8c8f57adc27aab3dbe94c4c7
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-26 22:55:40 -0300

    :bug: fix: return statement in service layer for user flow fixed

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -1,12 +1,19 @@
 
-class UserService {
+export class UserService {
+
+  constructor() {}
 
   async userSignUp(payload: any) {
     const result = 'Signing user up';
+    return result;
   }
 
   async userSignIn(payload: any) {
     const result = 'Signing user in';
+    return result;
   }
 
 }
+
+export default new UserService();
+exports = { UserService }

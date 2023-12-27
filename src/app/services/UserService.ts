f55ede5de5f56aa02cc44e973a70024b5bdcf20b
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-26 22:20:05 -0300

    :sparkles: feat: service layer added to user flow

---
+++ src/app/services/UserService.ts
@@ -0,0 +1,12 @@
+
+class UserService {
+
+  async userSignUp(payload: any) {
+    const result = 'Signing user up';
+  }
+
+  async userSignIn(payload: any) {
+    const result = 'Signing user in';
+  }
+
+}

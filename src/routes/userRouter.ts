de36145186380f0bc8b6fb9f271711f95a300d0c
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-27 14:51:18 -0300

    :sparkles: feat: user login route implemented validation middleware

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,12 @@
 import Router from 'express';
 import UserController from '../app/controllers/UserController';
 import ValidationMiddleware from '../app/middlewares/ValidationMiddleware';
-import AuthenticationMiddleware from '../app/middlewares/AuthenticationMiddleware';
+import ValidateUser from '../app/validations/User/ValidateUser';
 
 const userRouter = Router();
 
-userRouter.post('/users/sign-up', ValidationMiddleware.validateUserPayload, UserController.userSignUp);
+userRouter.post('/users/sign-up', ValidationMiddleware.validateUserPayload(ValidateUser.createUser()), UserController.userSignUp);
 
-userRouter.post('/users/sign-in', UserController.userSignIn);
+userRouter.post('/users/sign-in', ValidationMiddleware.validateUserPayload(ValidateUser.loginUser()), UserController.userSignIn);
 
 export default userRouter;

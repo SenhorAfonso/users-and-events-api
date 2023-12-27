137931ba542d883e40bc126567685633ba42e018
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-27 13:33:05 -0300

    :recycle: refactor: authentication middleware finished

--- src/app/middlewares/AuthenticationMiddleware.ts
+++ src/app/middlewares/AuthenticationMiddleware.ts
@@ -3,13 +3,30 @@ import jwt from "jsonwebtoken";
 
 class AuthenticationMiddleware {
 
-  static async AuthenticateSignIn(
+  async AuthenticateSignIn(
     req: Request,
     res: Response,
     next: NextFunction
   ) {
-    console.log(req.headers.authorization)
-    next()
+    const authHeader = req.headers.authorization;
+
+    if (this.authHeaderIsNotValid(authHeader)) {
+      return res.status(401).json({ msg: 'Unauthorized' });
+    }
+    
+    const token = authHeader!.split(' ')[1];
+    
+    try {
+      jwt.verify(token, 'SECRETKEY');
+      next();
+    } catch (error) {
+      return res.status(401).json({ msg: 'Unauthorized' });
+    }
+    
+  }
+
+  authHeaderIsNotValid(authHeader: any) {
+    return !authHeader || !authHeader.startsWith('Bearer ');
   }
 
 }

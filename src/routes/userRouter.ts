363c23c587ff85db2c57f9d1d86ecfdbb8d4beba
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-27 13:07:51 -0300

    :recycle: refactor: authentication middleware implemented in user sign-in route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,12 @@
 import Router from 'express';
 import UserController from '../app/controllers/UserController';
 import ValidationMiddleware from '../app/middlewares/ValidationMiddleware';
+import AuthenticationMiddleware from '../app/middlewares/AuthenticationMiddleware';
 
 const userRouter = Router();
 
 userRouter.post('/users/sign-up', ValidationMiddleware.validateUserPayload, UserController.userSignUp);
 
-userRouter.post('/users/sign-in', UserController.userSignIn);
+userRouter.post('/users/sign-in', AuthenticationMiddleware.AuthenticateSignIn, UserController.userSignIn);
 
 export default userRouter;

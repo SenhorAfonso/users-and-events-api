0b024e4a6641aa8530b550a6f9dfb7e986cee33d
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-27 15:45:55 -0300

    :recycle: refactor: user controller's returns simplified

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -4,13 +4,13 @@ import userService from "../services/UserService";
 class UserController {
 
   async userSignUp(req: Request, res: Response) {
-    const result = await userService.userSignUp(req.body);
-    res.send({ result });
+    const { result } = await userService.userSignUp(req.body);
+    res.send({ data: result });
   }
 
   async userSignIn(req: Request, res: Response) {
-    const result = await userService.userSignIn(req.body);
-    res.send({ result })
+    const { result, token } = await userService.userSignIn(req.body);
+    res.send({ data: result, token })
   }
 
 }

44efbc91b9dfa9375095c428bfd3394272e5916e
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-27 17:57:31 -0300

    :sparkles: feat: added service layer for event flow

---
+++ src/app/services/EventService.ts
@@ -0,0 +1,29 @@
+
+class EventService {
+
+  createEvent(payload: any) {
+    const result = 'Create an event';
+    return result;
+  }
+
+  getEvents() {
+    const result = 'get an event';
+    return result;
+  }
+
+  getSingleEvents(payload: any) {
+    const result = 'get single event';
+    return result;
+  }
+
+  deleteEvent(payload: any) {
+    const result = 'delete an event';
+    return result;
+  }
+
+  deleteSingleEvent(payload: any) {
+    const result = 'delete single event';
+    return result;
+  }
+
+}

ec1fe613f3edc26f5d9e967321685802d46effb7
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-26 23:08:08 -0300

    :sparkles: feat: repository layer added in user flow

---
+++ src/app/repositories/UserRepository.ts
@@ -0,0 +1,14 @@
+
+class UserRepository {
+
+  async create() {
+    const result = 'Signing user up';
+    return result;
+  }
+
+  async login() {
+    const result = 'Signing user in';
+    return result;
+  }
+
+}

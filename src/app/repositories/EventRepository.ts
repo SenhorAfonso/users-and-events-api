91bb80ec780da09634387a4323ade99be792e157
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-28 19:03:19 -0300

    :recycle: refactor: events get all method reformuled

--- src/app/repositories/EventRepository.ts
+++ src/app/repositories/EventRepository.ts
@@ -30,22 +30,30 @@ class EventRepository {
   }
 
   async getAll(queryObject: IEventQueryParams) {
+    let status: number = 0;
+    let msg: string = '';
+    let success: boolean = true;
+    let result: mongoose.Document[];
     let { limit, page, sort, skip, ...query } = queryObject;
+
     limit = limit ?? 3;
     page = page ?? 1;
     sort = sort ?? 'asc';
     skip = (page - 1) * limit || skip || 0;
 
-    const result = await eventSchema.find(query)
+    result = await eventSchema.find(query)
       .sort({ description: sort })
       .skip(skip)
       .limit(limit);
 
     if (result.length === 0) {
-      throw new NotFoundError('Not found');
+      throw new NotFoundError('Not Found');
     }
 
-    return result;
+    status = StatusCodes.OK;
+    msg = 'Successful operation';
+
+    return { success, status, msg, result };
   }
 
   async getSingle(payload: any) {

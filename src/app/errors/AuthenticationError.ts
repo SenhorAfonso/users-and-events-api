f3e12c7eac52292db430d8cd8e7fecfe2077f683
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-28 15:23:17 -0300

    :recycle: refactor: authenticationError name property removed

--- src/app/errors/AuthenticationError.ts
+++ src/app/errors/AuthenticationError.ts
@@ -2,12 +2,10 @@ import StatusCodes from "http-status-codes";
 
 class AuthenticationError extends Error {
   private status: number;
-  public name: string;
 
   constructor(message: string) {
     super(message);
     this.status = StatusCodes.UNAUTHORIZED;
-    this.name = 'Unauthorized'
   }
 }
 

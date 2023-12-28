bacf6a6e72ab6f38d2581ebdd779bae731a5a80b
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-28 13:15:56 -0300

    :sparkles: feat: error handling middleware can treat joi validation error

--- src/app/middlewares/ErrorHandlingMiddleware.ts
+++ src/app/middlewares/ErrorHandlingMiddleware.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+import StatusCodes from "http-status-codes";
 
 class ErrorHandlingMiddleware {
 
@@ -8,9 +10,30 @@ class ErrorHandlingMiddleware {
     res: Response,
     next: NextFunction
   ) {
-    if (error) {
-      res.send('erro');
+    if (error instanceof Joi.ValidationError) {
+      const joiErrors = ErrorHandlingMiddleware.formatJoiValidationErrors(error);
+
+      let type = error.name;
+
+      res.status(StatusCodes.BAD_REQUEST).json({ type, errors: joiErrors })
+    } else {
+      res.send(error.message);
     }
+  }
+
+  static formatJoiValidationErrors(error: Joi.ValidationError) {
+    let type = error.name;
+    let errors: Array<{
+      resource: string,
+      message: string
+    }> = [];
+
+    errors = error.details.map(element => ({
+      resource: element.path.toString(),
+      message: element.message
+    }));
+
+    return { type, errors };
   }
 
 }

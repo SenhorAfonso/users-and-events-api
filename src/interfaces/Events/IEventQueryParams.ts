9e1e61d0878af5b980de4b54e2c99ef417cc4214
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-29 11:37:29 -0300

    :recycle: refactor: IEventQueryParams interface renamed

--- src/interfaces/Events/IEventQueryParams.ts
+++
@@ -1,9 +0,0 @@
-export default interface IEventQueryParams {
-  _id?: string;
-  description?: string;
-  dayOfWeek?: 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday';
-  limit?: number;
-  sort?: 'asc' | 'desc';
-  page?: number;
-  skip?: number;
-}

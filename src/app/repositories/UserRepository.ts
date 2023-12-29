fc921d34004257cf928f3a62254834fe4d73e816
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-29 18:11:22 -0300

    :recycle: refactor: user sign up route response reformuled

--- src/app/repositories/UserRepository.ts
+++ src/app/repositories/UserRepository.ts
@@ -1,12 +1,22 @@
 import userSchema from "../schemas/userSchema";
 import ICreateUserPayload from "../../interfaces/ICreateUserPayload";
 import ILoginUserPayload from "../../interfaces/ILoginUserPayload";
+import StatusCodes from "http-status-codes";
+import InternalServerError from "../errors/InternalServerError";
 
 class UserRepository {
 
   async create(payload: ICreateUserPayload) {
-    const result = await userSchema.create(payload);
-    return result;
+    const status: number = StatusCodes.CREATED;
+    const success: boolean = true;
+    const message: string = 'User created';
+
+    try {
+      const result = await userSchema.create(payload);
+      return { success, status, message, result };
+    } catch (error) {
+      throw new InternalServerError();
+    }
   }
 
   async login(payload: ILoginUserPayload) {

577cbcff8f1fce108c945f02851b38dc1face6d6
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-29 23:09:38 -0300

    :sparkles: feat: added test for invalid lastName key and value

--- src/tests/userTest/userValidation.test.ts
+++ src/tests/userTest/userValidation.test.ts
@@ -47,4 +47,41 @@ describe('Validation payload for users sign-up route', () => {
     expect(error.path).toStrictEqual(["firstName"]);
   })
 
+  it('Should return a "lastName length must be at least 5 characters long" error', () => {
+    const userPayload = {
+      "firstName": "Pedro",
+      "lastName": "Afon",
+      "birthDate": "2023-12-27",
+      "city": "Maringá",
+      "country": "Brasil",
+      "email": "pedroafonso@gmail.com",
+      "password": "password123",
+      "confirmPassword": "password123"
+    }
+
+    const error = testValidateObject(ValidateUser.createUser(), userPayload).error?.details[0]!;
+
+    expect(error.message).toMatch('\"lastName\" length must be at least 5 characters long');
+    expect(error.path).toStrictEqual(["lastName"]);
+
+  });
+
+  it('Should return a "lastName is required" error', () => {
+    const userPayload = {
+      "firstName": "Pedro",
+      "lastNaame": "Afonso",
+      "birthDate": "2023-12-27",
+      "city": "Maringá",
+      "country": "Brasil",
+      "email": "pedroafonso@gmail.com",
+      "password": "password123",
+      "confirmPassword": "password123"
+    }
+
+    const error = testValidateObject(ValidateUser.createUser(), userPayload).error?.details[0]!;
+
+    expect(error.message).toMatch('\"lastName\" is required');
+    expect(error.path).toStrictEqual(["lastName"]);
+  })
+
 })

b8eb26e0e34f866473e67c262b0c921aa4e2719e
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-29 23:15:50 -0300

    :sparkles: feat: added test for invalid birthDate key and value

--- src/tests/userTest/userValidation.test.ts
+++ src/tests/userTest/userValidation.test.ts
@@ -83,5 +83,41 @@ describe('Validation payload for users sign-up route', () => {
     expect(error.message).toMatch('\"lastName\" is required');
     expect(error.path).toStrictEqual(["lastName"]);
   })
+  it('Should return a "birthDate must be in YYYY-MM-DD format" error', () => {
+    const userPayload = {
+      "firstName": "Pedro",
+      "lastName": "Afonso",
+      "birthDate": "2023/12/27",
+      "city": "Maringá",
+      "country": "Brasil",
+      "email": "pedroafonso@gmail.com",
+      "password": "password123",
+      "confirmPassword": "password123"
+    }
+
+    const error = testValidateObject(ValidateUser.createUser(), userPayload).error?.details[0]!;
+
+    expect(error.message).toMatch('\"birthDate\" must be in YYYY-MM-DD format');
+    expect(error.path).toStrictEqual(["birthDate"]);
+
+  });
+
+  it('Should return a "birthDate is required" error', () => {
+    const userPayload = {
+      "firstName": "Pedro",
+      "lastName": "Afonso",
+      "birthDaate": "2023-12-27",
+      "city": "Maringá",
+      "country": "Brasil",
+      "email": "pedroafonso@gmail.com",
+      "password": "password123",
+      "confirmPassword": "password123"
+    }
+
+    const error = testValidateObject(ValidateUser.createUser(), userPayload).error?.details[0]!;
+
+    expect(error.message).toMatch('\"birthDate\" is required');
+    expect(error.path).toStrictEqual(["birthDate"]);
+  })
 
 })

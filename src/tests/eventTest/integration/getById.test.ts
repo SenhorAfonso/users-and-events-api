ef4d67d4648d477f8374aa2368f370d24a0cb5d1
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-30 20:33:07 -0300

    :sparkles: feat: added test case for 404 status code in getById events route

--- src/tests/eventTest/integration/getById.test.ts
+++ src/tests/eventTest/integration/getById.test.ts
@@ -75,6 +75,33 @@ describe("Check for getAll event's route http response", () => {
       
     })
 
+    it('Should return 404 status code if payload is valid but there is no register in database', async () => {
+      const userSignUp = {
+        "firstName": "Pedro",
+        "lastName": "Afonso",
+        "birthDate": "2023-12-27",
+        "city": "Maringá",
+        "country": "Brasil",
+        "email": "pedroafonso@gmail.com",
+        "password": "password123",
+        "confirmPassword": "password123"
+      };
+  
+      const { email, password } = await TestUtils.signUpUser(userSignUp);
+      const token = await TestUtils.loginUser({ email, password });
+      await TestUtils.createEvent({ dayOfWeek: "sunday", description: 'event 1' }, token);
+      const eventId = '658f28878a2be8dca4627463';
+    
+      const response = await request(server)
+        .get(`/api/v1/users-and-events/event/${eventId}`)
+        .auth(token, { type: 'bearer' });
+
+      expect(response.status).toBe(StatusCodes.NOT_FOUND);
+      expect(response.body.error).toBe('Not Found');
+      expect(response.body.message).toBe('Not Found');
+      
+    })
+
 
   })
 

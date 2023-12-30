38d396c49ff3a8d27a1f85108c7f09fcf2fee4d2
Pedro Afonso <sirafonso005896@outlook.com>
2023-12-29 23:37:14 -0300

    :sparkles: feat: added test for invalid confirmPassword key and value

--- src/tests/userTest/userValidation.test.ts
+++ src/tests/userTest/userValidation.test.ts
@@ -268,4 +268,41 @@ describe('Validation payload for users sign-up route', () => {
     expect(error.path).toStrictEqual(["password"]);
   })
 
+  it('Should return an "invalid pattern" error to confirmPassword less than 6 characters', () => {
+    const userPayload = {
+      "firstName": "Pedro",
+      "lastName": "Afonso",
+      "birthDate": "2023-12-27",
+      "city": "Maringá",
+      "country": "Brasil",
+      "email": "pedroafonso@gmail.com",
+      "password": "password123",
+      "confirmPassword": "p123"
+    }
+
+    const error = testValidateObject(ValidateUser.createUser(), userPayload).error?.details[0]!;
+
+    expect(error.message).toMatch('\"confirmPassword\" with value \"p123\" fails to match the required pattern: /^[a-zA-Z0-9]{6,30}$/');
+    expect(error.path).toStrictEqual(["confirmPassword"]);
+
+  });
+
+  it('Should return a "confirmPassword is required" error', () => {
+    const userPayload = {
+      "firstName": "Pedro",
+      "lastName": "Afonso",
+      "birthDate": "2023-12-27",
+      "city": "Maringá",
+      "country": "Brasil",
+      "email": "pedroafonso@gmail.com",
+      "password": "password123",
+      "confirmaPassworda": "password123"
+    }
+
+    const error = testValidateObject(ValidateUser.createUser(), userPayload).error?.details[0]!;
+
+    expect(error.message).toMatch('\"confirmPassword\" is required');
+    expect(error.path).toStrictEqual(["confirmPassword"]);
+  })
+
 })

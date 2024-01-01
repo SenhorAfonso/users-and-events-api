aeda9356262a8c510acd115ae519cf576653e3bb
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-01 20:08:02 -0300

    :recycle: refactor: error test suit reorganized

--- src/tests/errors/errors.test.ts
+++ src/tests/errors/errors.test.ts
@@ -7,75 +7,87 @@ import NotFoundError from "../../app/errors/NotFoundError";
 
 describe('Check if errors status code, name and message are correct', () => {
 
-  it('AuthenticationError should be working properly even without a argument in constructor', () => {
+  describe('AuthenticationError should', () => {
 
-    try {
-      throw new AuthenticationError();
-    } catch (error: any) {
-      expect(error).toStrictEqual(new AuthenticationError());
-      expect(error.name).toBe('Unauthorized');
-      expect(error.message).toBe('Not Authenticated');
-    }
+    it('AuthenticationError should be working properly even without a argument in constructor', () => {
 
-  })
-
-  it('AuthenticationError should have a different message if it is passed in constructor', () => {
-
-    try {
-      throw new AuthenticationError('Different message');
-    } catch (error: any) {
-      expect(error).toStrictEqual(new AuthenticationError('Different message'));
-      expect(error.name).toBe('Unauthorized');
-      expect(error.message).toBe('Different message');
-    }
-
-  })
-
-  it('BadRequestError should be working properly even without a argument in constructor', () => {
-
-    try {
-      throw new BadRequestError();
-    } catch (error: any) {
-      expect(error).toStrictEqual(new BadRequestError());
-      expect(error.name).toBe('Bad Request');
-      expect(error.message).toBe('Invalid Input');
-    }
+      try {
+        throw new AuthenticationError();
+      } catch (error: any) {
+        expect(error).toStrictEqual(new AuthenticationError());
+        expect(error.name).toBe('Unauthorized');
+        expect(error.message).toBe('Not Authenticated');
+      }
+  
+    })
+  
+    it('AuthenticationError should have a different message if it is passed in constructor', () => {
+  
+      try {
+        throw new AuthenticationError('Different message');
+      } catch (error: any) {
+        expect(error).toStrictEqual(new AuthenticationError('Different message'));
+        expect(error.name).toBe('Unauthorized');
+        expect(error.message).toBe('Different message');
+      }
+  
+    })
 
   })
 
-  it('BadRequestError should have a different message if it is passed in constructor', () => {
+  describe('BadRequestError should', () => {
 
-    try {
-      throw new BadRequestError('Different message');
-    } catch (error: any) {
-      expect(error).toStrictEqual(new BadRequestError('Different message'));
-      expect(error.name).toBe('Bad Request');
-      expect(error.message).toBe('Different message');
-    }
+    it('BadRequestError should be working properly even without a argument in constructor', () => {
+
+      try {
+        throw new BadRequestError();
+      } catch (error: any) {
+        expect(error).toStrictEqual(new BadRequestError());
+        expect(error.name).toBe('Bad Request');
+        expect(error.message).toBe('Invalid Input');
+      }
+  
+    })
+  
+    it('BadRequestError should have a different message if it is passed in constructor', () => {
+  
+      try {
+        throw new BadRequestError('Different message');
+      } catch (error: any) {
+        expect(error).toStrictEqual(new BadRequestError('Different message'));
+        expect(error.name).toBe('Bad Request');
+        expect(error.message).toBe('Different message');
+      }
+  
+    })
 
   })
 
-  it('DuplicatedValueError should be working properly even without a argument in constructor', () => {
+  describe('DuplicatedValueError should', () => {
 
-    try {
-      throw new DuplicatedValueError();
-    } catch (error: any) {
-      expect(error).toStrictEqual(new DuplicatedValueError());
-      expect(error.name).toBe('Duplicated Value');
-      expect(error.message).toBe('Input value already exists');
-    }
+    it('be working properly even without a argument in constructor', () => {
 
-  })
-
-  it('DuplicatedValueError should have a different message if it is passed in constructor', () => {
-
-    try {
-      throw new DuplicatedValueError('Different message');
-    } catch (error: any) {
-      expect(error).toStrictEqual(new DuplicatedValueError('Different message'));
-      expect(error.name).toBe('Duplicated Value');
-      expect(error.message).toBe('Different message');
-    }
+      try {
+        throw new DuplicatedValueError();
+      } catch (error: any) {
+        expect(error).toStrictEqual(new DuplicatedValueError());
+        expect(error.name).toBe('Duplicated Value');
+        expect(error.message).toBe('Input value already exists');
+      }
+  
+    })
+  
+    it('have a different message if it is passed in constructor', () => {
+  
+      try {
+        throw new DuplicatedValueError('Different message');
+      } catch (error: any) {
+        expect(error).toStrictEqual(new DuplicatedValueError('Different message'));
+        expect(error.name).toBe('Duplicated Value');
+        expect(error.message).toBe('Different message');
+      }
+  
+    })
 
   })
 

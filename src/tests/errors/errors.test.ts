72b9c882f44ed81966a0f9893e29bc7428cb185a
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-01 20:00:15 -0300

    :sparkles: feat: added test case for handle custom duplicatedValue error throw

--- src/tests/errors/errors.test.ts
+++ src/tests/errors/errors.test.ts
@@ -1,6 +1,7 @@
 import ApiErrors from "../../app/errors/ApiErrors";
 import AuthenticationError from "../../app/errors/AuthenticationError"
 import BadRequestError from "../../app/errors/BadRequestError";
+import DuplicatedValueError from "../../app/errors/DuplicatedValueError";
 
 describe('Check if errors status code, name and message are correct', () => {
 
@@ -47,6 +48,30 @@ describe('Check if errors status code, name and message are correct', () => {
     } catch (error: any) {
       expect(error).toStrictEqual(new BadRequestError('Different message'));
       expect(error.name).toBe('Bad Request');
+      expect(error.message).toBe('Different message');
+    }
+
+  })
+
+  it('DuplicatedValueError should be working properly even without a argument in constructor', () => {
+
+    try {
+      throw new DuplicatedValueError();
+    } catch (error: any) {
+      expect(error).toStrictEqual(new DuplicatedValueError());
+      expect(error.name).toBe('Duplicated Value');
+      expect(error.message).toBe('Input value already exists');
+    }
+
+  })
+
+  it('DuplicatedValueError should have a different message if it is passed in constructor', () => {
+
+    try {
+      throw new DuplicatedValueError('Different message');
+    } catch (error: any) {
+      expect(error).toStrictEqual(new DuplicatedValueError('Different message'));
+      expect(error.name).toBe('Duplicated Value');
       expect(error.message).toBe('Different message');
     }
 

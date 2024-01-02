7c0bdfd0e7a8c248973c32e6533080201671a3c4
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-02 15:50:56 -0300

    :recycle: refactor: getAll test beautyfied

--- src/tests/eventTest/integration/getAllEvents.test.ts
+++ src/tests/eventTest/integration/getAllEvents.test.ts
@@ -1,48 +1,47 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import eventSchema from "../../../app/schemas/eventSchema";
-import userSchema from "../../../app/schemas/userSchema";
-import serverConfig from "../../../config/config";
-import request from "supertest";
-import server from "../../../server";
-import StatusCodes from "http-status-codes";
-import IQueryByObject from "../../../interfaces/Events/IQueryByObject";
-import createQueryByObject from "../../../app/utils/createQueryByObject";
-import TestUtils from "../../../app/utils/testUtils/TestUtils";
-import IQueryByObjectParams from "../../../interfaces/Events/IQueryByObjectParams";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import StatusCodes from 'http-status-codes';
+import eventSchema from '../../../app/schemas/eventSchema';
+import userSchema from '../../../app/schemas/userSchema';
+import server from '../../../server';
+import IQueryByObject from '../../../interfaces/Events/IQueryByObject';
+import createQueryByObject from '../../../app/utils/createQueryByObject';
+import TestUtils from '../../../app/utils/testUtils/TestUtils';
+import IQueryByObjectParams from '../../../interfaces/Events/IQueryByObjectParams';
 
 let mongoServer: MongoMemoryServer;
 
-describe("Check for getAll event's route http response", () => {
+describe('Check for getAll event\'s route http response', () => {
 
   beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
     const mongoURI = await mongoServer.getUri();
 
     await mongoose.connect(mongoURI);
-  })
+  });
 
   afterEach(async () => {
     await userSchema.collection.drop();
     await eventSchema.collection.drop();
-  })
+  });
 
   afterAll(async () => {
     await mongoServer.stop();
     await mongoose.disconnect();
-  })
+  });
 
   it('Should return 200 status code if payload is valid and user is logged', async () => {
 
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     await request(server)
@@ -80,19 +79,19 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.success).toBeTruthy();
     expect(response.body.data).toHaveLength(1);
 
-  })
-  
+  });
+
   it('Should return 401 status code if payload is valid and user is not logged', async () => {
 
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     await request(server)
@@ -128,19 +127,19 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.error).toBe('Unauthorized');
     expect(response.body.message).toBe('Not Authenticated');
 
-  })
+  });
 
   it('Should return 404 status code if payload is valid but there is no register in database', async () => {
 
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     await request(server)
@@ -181,31 +180,31 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.message).toBe('Not Found');
     expect(response.body.data).toBeUndefined();
 
-  })
+  });
 
   it('Should return only 3 events (dayOfWeek property check)', async () => {
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     const { email, password } = await TestUtils.signUpUser(userSignUp);
     const token = await TestUtils.loginUser({ email, password });
 
-    await TestUtils.createEvent({ dayOfWeek: "sunday", description: 'event 1' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 2' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 3' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 4' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'sunday', description: 'event 1' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 2' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 3' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 4' }, token);
 
     const queryObjectParams: IQueryByObjectParams = {
-      dayOfWeek: "monday"
-    }
+      dayOfWeek: 'monday'
+    };
 
     const queryObject = createQueryByObject(queryObjectParams);
 
@@ -218,31 +217,31 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.message).toBe('Successful operation');
     expect(response.body.data).toHaveLength(3);
 
-  })
+  });
 
   it('Should return only 1 event (description property check)', async () => {
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     const { email, password } = await TestUtils.signUpUser(userSignUp);
     const token = await TestUtils.loginUser({ email, password });
 
-    await TestUtils.createEvent({ dayOfWeek: "sunday", description: 'event 1' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 2' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 3' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 4' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'sunday', description: 'event 1' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 2' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 3' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 4' }, token);
 
     const queryObjectParams: IQueryByObjectParams = {
-      description: "event 1"
-    }
+      description: 'event 1'
+    };
 
     const queryObject = createQueryByObject(queryObjectParams);
 
@@ -255,32 +254,32 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.message).toBe('Successful operation');
     expect(response.body.data).toHaveLength(1);
 
-  })
+  });
 
   it('Should return only 1 event (limit property check)', async () => {
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     const { email, password } = await TestUtils.signUpUser(userSignUp);
     const token = await TestUtils.loginUser({ email, password });
 
-    await TestUtils.createEvent({ dayOfWeek: "sunday", description: 'event 1' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 2' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 3' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 4' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'sunday', description: 'event 1' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 2' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 3' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 4' }, token);
 
     const queryObjectParams: IQueryByObjectParams = {
-      dayOfWeek: "monday",
+      dayOfWeek: 'monday',
       limit: 1
-    }
+    };
 
     const queryObject = createQueryByObject(queryObjectParams);
 
@@ -293,32 +292,32 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.message).toBe('Successful operation');
     expect(response.body.data).toHaveLength(1);
 
-  })
+  });
 
   it('Should return the last event (sort property check)', async () => {
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     const { email, password } = await TestUtils.signUpUser(userSignUp);
     const token = await TestUtils.loginUser({ email, password });
 
-    await TestUtils.createEvent({ dayOfWeek: "sunday", description: 'event 1' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 2' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 3' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 4' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'sunday', description: 'event 1' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 2' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 3' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 4' }, token);
 
     const queryObjectParams: IQueryByObjectParams = {
       sort: 'desc',
       limit: 1
-    }
+    };
 
     const queryObject = createQueryByObject(queryObjectParams);
 
@@ -332,31 +331,31 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.data).toHaveLength(1);
     expect(response.body.data[0].description).toBe('event 4');
 
-  })
+  });
 
   it('Should return the last page 1 event (page property check)', async () => {
     const userSignUp = {
-      "firstName": "Pedro",
-      "lastName": "Afonso",
-      "birthDate": "2023-12-27",
-      "city": "Maringá",
-      "country": "Brasil",
-      "email": "pedroafonso@gmail.com",
-      "password": "password123",
-      "confirmPassword": "password123"
+      'firstName': 'Pedro',
+      'lastName': 'Afonso',
+      'birthDate': '2023-12-27',
+      'city': 'Maringá',
+      'country': 'Brasil',
+      'email': 'pedroafonso@gmail.com',
+      'password': 'password123',
+      'confirmPassword': 'password123'
     };
 
     const { email, password } = await TestUtils.signUpUser(userSignUp);
     const token = await TestUtils.loginUser({ email, password });
 
-    await TestUtils.createEvent({ dayOfWeek: "sunday", description: 'event 1' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 2' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 3' }, token);
-    await TestUtils.createEvent({ dayOfWeek: "monday", description: 'event 4' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'sunday', description: 'event 1' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 2' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 3' }, token);
+    await TestUtils.createEvent({ dayOfWeek: 'monday', description: 'event 4' }, token);
 
     const queryObjectParams: IQueryByObjectParams = {
       page: 2
-    }
+    };
 
     const queryObject = createQueryByObject(queryObjectParams);
 
@@ -369,6 +368,6 @@ describe("Check for getAll event's route http response", () => {
     expect(response.body.message).toBe('Successful operation');
     expect(response.body.data).toHaveLength(1);
 
-  })
+  });
 
 });

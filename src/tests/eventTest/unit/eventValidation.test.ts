46ee92d6f49630b981d79bad584e706b6395b7dc
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-02 16:12:50 -0300

    :recycle: refactor: eventValidation beautyfied

--- src/tests/eventTest/unit/eventValidation.test.ts
+++ src/tests/eventTest/unit/eventValidation.test.ts
@@ -1,5 +1,5 @@
-import ValidateEvent from "../../../app/validations/Event/ValidateEvent";
-import TestUtils from "../../../app/utils/testUtils/TestUtils";
+import ValidateEvent from '../../../app/validations/Event/ValidateEvent';
+import TestUtils from '../../../app/utils/testUtils/TestUtils';
 
 describe('Validation payload for create event route', () => {
 
@@ -7,57 +7,57 @@ describe('Validation payload for create event route', () => {
 
     it('Should return a ""dayOfWeek" must be one of <list>" error', () => {
       const eventPayload = {
-        "dayOfWeek": "invalid",
-        "description": "Evento 1"
-      }
+        'dayOfWeek': 'invalid',
+        'description': 'Evento 1'
+      };
 
-      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error?.details[0]!;
+      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error!.details[0]!;
 
-      expect(error.message).toMatch('\"dayOfWeek\" must be one of [sunday, monday, tuesday, wednesday, thursday, friday, saturday]');
+      expect(error.message).toMatch('"dayOfWeek" must be one of [sunday, monday, tuesday, wednesday, thursday, friday, saturday]');
       expect(error.path).toStrictEqual(['dayOfWeek']);
 
-    })
+    });
 
     it('Should return a ""dayOfWeek" is required" error', () => {
       const eventPayload = {
-        "invalid": "monday",
-        "description": "Evento 1"
-      }
+        'invalid': 'monday',
+        'description': 'Evento 1'
+      };
 
-      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error?.details[0]!;
+      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error!.details[0]!;
 
-      expect(error.message).toMatch('\"dayOfWeek\" is required');
+      expect(error.message).toMatch('"dayOfWeek" is required');
       expect(error.path).toStrictEqual(['dayOfWeek']);
 
-    })
-  })
+    });
+  });
 
   describe('Validade description field and value', () => {
 
-    it('Should return a \"description\" is not allowed to be empty" error', () => {
+    it('Should return a "description" is not allowed to be empty" error', () => {
       const eventPayload = {
-        "dayOfWeek": "monday",
-        "description": ""
-      }
+        'dayOfWeek': 'monday',
+        'description': ''
+      };
 
-      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error?.details[0]!;
+      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error!.details[0]!;
 
-      expect(error.message).toMatch('\"description\" is not allowed to be empty');
+      expect(error.message).toMatch('"description" is not allowed to be empty');
       expect(error.path).toStrictEqual(['description']);
 
-    })
+    });
 
     it('Should return a "invalid is not allowed" error for extra fields', () => {
       const eventPayload = {
-        "dayOfWeek": "monday",
-        "invalid": "Evento 1"
-      }
+        'dayOfWeek': 'monday',
+        'invalid': 'Evento 1'
+      };
 
-      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error?.details[0]!;
+      const error = TestUtils.validateObject(ValidateEvent.createEvent(), eventPayload).error!.details[0]!;
 
-      expect(error.message).toMatch('\"invalid\" is not allowed');
+      expect(error.message).toMatch('"invalid" is not allowed');
       expect(error.path).toStrictEqual(['invalid']);
 
-  })
-  })
-})
+    });
+  });
+});

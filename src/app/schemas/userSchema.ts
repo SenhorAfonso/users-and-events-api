50e8bd29e37bdf51dcb17b96f0f15fc3fa74e0c6
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-02 14:09:40 -0300

    :recycle: refactor: userSchema beautyfied

--- src/app/schemas/userSchema.ts
+++ src/app/schemas/userSchema.ts
@@ -1,15 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+const MIN_LENGHT_USER_NAME: number = 5;
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    minLenght: [5, 'User first name need to be more than 5 characteres.'],
+    minLenght: [MIN_LENGHT_USER_NAME, `User first name need to be more than ${MIN_LENGHT_USER_NAME} characteres.`],
     require: [true, 'User first name is required.'],
     trim: true
   },
   lastName: {
     type: String,
-    minLenght: [5, 'User last name need to be more than 5 characteres.'],
+    minLenght: [MIN_LENGHT_USER_NAME, `User last name need to be more than ${MIN_LENGHT_USER_NAME} characteres.`],
     require: [true, 'User last name is required.'],
     trim: true
   },
@@ -17,7 +19,7 @@ const userSchema = new mongoose.Schema({
     type: Date,
     required: [true, 'Must provide a birth date'],
     trim: true,
-    match: [/\d{4}-\d{2}-\d{2}/, 'Please fill a valid birth date']
+    match: [/\d{4}-\d{2}-\d{2}/u, 'Please fill a valid birth date']
   },
   city: {
     type: String,
@@ -31,17 +33,17 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    match: [/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please fill a valid email address'],
+    match: [/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/u, 'Please fill a valid email address'],
     require: [true, 'User email is required.'],
     trim: true
   },
   password: {
     type: String,
-    match: [/^[a-zA-Z0-9]{6,30}$/, 'Please fill a valid password'],
+    match: [/^[a-zA-Z0-9]{6,30}$/u, 'Please fill a valid password'],
     require: [true, 'User password is required.'],
     trim: true
   },
 
-})
+});
 
 export default mongoose.model('userSchema', userSchema);

1b9b223e16f76a354e56b9b68a8136875265770d
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-02 16:57:51 -0300

    :recycle: refactor: test for utils functions beautyfied

--- src/tests/utils/utilsFunctions.test.ts
+++ src/tests/utils/utilsFunctions.test.ts
@@ -1,8 +1,8 @@
-import createQueryById from "../../app/utils/createQueryById"
-import createQueryByObject from "../../app/utils/createQueryByObject";
-import IQueryByIdParams from "../../interfaces/Events/IQueryByIdParams"
-import IQueryByObjectParams from "../../interfaces/Events/IQueryByObjectParams";
-import resultIsEmpty from "../../app/utils/resultIsEmpty";
+import createQueryById from '../../app/utils/createQueryById';
+import createQueryByObject from '../../app/utils/createQueryByObject';
+import IQueryByIdParams from '../../interfaces/Events/IQueryByIdParams';
+import IQueryByObjectParams from '../../interfaces/Events/IQueryByObjectParams';
+import resultIsEmpty from '../../app/utils/resultIsEmpty';
 
 describe('Create query by id function', () => {
 
@@ -13,7 +13,7 @@ describe('Create query by id function', () => {
 
     expect(queryObject).toEqual({});
 
-  })
+  });
 
   it('Should return a object with a id number if input object has a id member', () => {
     const id: string = 'testId';
@@ -22,9 +22,9 @@ describe('Create query by id function', () => {
     const queryObject = createQueryById(queryObjectParams);
 
     expect(queryObject).toEqual({ _id: id });
-  })
+  });
 
-})
+});
 
 describe('Create query by object', () => {
 
@@ -32,145 +32,145 @@ describe('Create query by object', () => {
 
     it('Should be in the query object if it was in the input object', () => {
       const queryObjectParams: IQueryByObjectParams = {
-        description: 'evento 1'  
-      }
+        description: 'evento 1'
+      };
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toHaveProperty('description', 'evento 1');
 
-    })
+    });
 
     it('Should not be in the query object if it was not in the input object', () => {
-      const queryObjectParams: IQueryByObjectParams = {}
+      const queryObjectParams: IQueryByObjectParams = {};
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toEqual({});
 
-    })
+    });
 
-  })
+  });
 
   describe('the "dayOfWeek" property', () => {
 
     it('Should be in the query object if it was in the input object', () => {
       const queryObjectParams: IQueryByObjectParams = {
-        dayOfWeek: 'monday'  
-      }
+        dayOfWeek: 'monday'
+      };
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toHaveProperty('dayOfWeek', 'monday');
 
-    })
+    });
 
     it('Should not be in the query object if it was not in the input object', () => {
-      const queryObjectParams: IQueryByObjectParams = {}
+      const queryObjectParams: IQueryByObjectParams = {};
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toEqual({});
 
-    })
+    });
 
-  })
+  });
 
   describe('the "limit" property', () => {
 
     it('Should be in the query object if it was in the input object', () => {
       const queryObjectParams: IQueryByObjectParams = {
-        limit: 1  
-      }
+        limit: 1
+      };
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toHaveProperty('limit', 1);
 
-    })
+    });
 
     it('Should not be in the query object if it was not in the input object', () => {
-      const queryObjectParams: IQueryByObjectParams = {}
+      const queryObjectParams: IQueryByObjectParams = {};
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toEqual({});
 
-    })
+    });
 
-  })
+  });
 
   describe('the "sort" property', () => {
 
     it('Should be in the query object if it was in the input object', () => {
       const queryObjectParams: IQueryByObjectParams = {
-        sort: 'asc'  
-      }
+        sort: 'asc'
+      };
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toHaveProperty('sort', 'asc');
 
-    })
+    });
 
     it('Should not be in the query object if it was not in the input object', () => {
-      const queryObjectParams: IQueryByObjectParams = {}
+      const queryObjectParams: IQueryByObjectParams = {};
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toEqual({});
 
-    })
+    });
 
-  })
+  });
 
   describe('the "page" property', () => {
 
     it('Should be in the query object if it was in the input object', () => {
       const queryObjectParams: IQueryByObjectParams = {
-        page: 1  
-      }
+        page: 1
+      };
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toHaveProperty('page', 1);
 
-    })
+    });
 
     it('Should not be in the query object if it was not in the input object', () => {
-      const queryObjectParams: IQueryByObjectParams = {}
+      const queryObjectParams: IQueryByObjectParams = {};
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toEqual({});
 
-    })
+    });
 
-  })
+  });
 
   describe('the "skip" property', () => {
 
     it('Should be in the query object if it was in the input object', () => {
       const queryObjectParams: IQueryByObjectParams = {
-        skip: 1  
-      }
+        skip: 1
+      };
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toHaveProperty('skip', 1);
 
-    })
+    });
 
     it('Should not be in the query object if it was not in the input object', () => {
-      const queryObjectParams: IQueryByObjectParams = {}
+      const queryObjectParams: IQueryByObjectParams = {};
 
       const queryObject = createQueryByObject(queryObjectParams);
 
       expect(queryObject).toEqual({});
 
-    })
+    });
 
-  })
+  });
 
   describe('if all properties was in the input object', () => {
 
@@ -181,8 +181,8 @@ describe('Create query by object', () => {
         limit: 1,
         sort: 'asc',
         page: 1,
-        skip: 1  
-      }
+        skip: 1
+      };
 
       const queryObject = createQueryByObject(queryObjectParams);
 
@@ -192,9 +192,9 @@ describe('Create query by object', () => {
       expect(queryObject).toHaveProperty('sort', 'asc');
       expect(queryObject).toHaveProperty('page', 1);
       expect(queryObject).toHaveProperty('skip', 1);
-    })
-  })
-})
+    });
+  });
+});
 
 describe('Validate if a target is empty/null', () => {
 
@@ -206,7 +206,7 @@ describe('Validate if a target is empty/null', () => {
       const isValid = resultIsEmpty(target);
 
       expect(isValid).toBeTruthy();
-    })
+    });
 
     it('Should return false if target is not empty', () => {
       const target: any[] = ['value'];
@@ -214,9 +214,9 @@ describe('Validate if a target is empty/null', () => {
       const isValid = resultIsEmpty(target);
 
       expect(isValid).toBeFalsy();
-    })
+    });
 
-  })
+  });
 
   describe('If target is an any type', () => {
 
@@ -226,7 +226,7 @@ describe('Validate if a target is empty/null', () => {
       const isValid = resultIsEmpty(target);
 
       expect(isValid).toBeTruthy();
-    })
+    });
 
     it('Should return false if target is defined', () => {
       const target: any = 'value';
@@ -234,8 +234,8 @@ describe('Validate if a target is empty/null', () => {
       const isValid = resultIsEmpty(target);
 
       expect(isValid).toBeFalsy();
-    })
+    });
 
-  })
+  });
 
-})
+});

c314d31dbfc77536ed818e38244c9a29d73be59a
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-02 11:00:15 -0300

    :recycle: refactor: user controller beautyfied

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,19 +1,20 @@
-import { Request, Response } from "express";
-import userService from "../services/UserService";
+import { Request, Response } from 'express';
+import userService from '../services/UserService';
 
 class UserController {
 
-  async userSignUp(req: Request, res: Response) {
+  static async userSignUp(req: Request, res: Response) {
     const { success, status, message, result } = await userService.userSignUp(req.body);
 
     res.status(status).json({ success, message, data: result });
   }
 
-  async userSignIn(req: Request, res: Response) {
+  static async userSignIn(req: Request, res: Response) {
     const { success, status, message, result } = await userService.userSignIn(req.body);
-    res.status(status).json({ success, message, data: result })
+
+    res.status(status).json({ success, message, data: result });
   }
 
 }
 
-export default new UserController();
+export default UserController;

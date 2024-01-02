ee386ce58271aff0949bdedcdf795b33ab7b52b9
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-02 17:03:53 -0300

    :recycle: refactor: app beautyfied

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
-import server from "./server";
-require("./infra/database/mongo/connectDB");
+import serverConfig from './config/config';
+import server from './server';
+
+require('./infra/database/mongo/connectDB');
 
 class App {
-  private port: number = 3000;
+  private port: number = Number(serverConfig.PORT!);
 
   constructor() {
-    this.start()
+    this.start();
   }
 
   private start() {
     server.listen(this.port, () => {
-      console.log(`Server is listening at ${this.port} port.`)
-    })
+      console.log(`Server is listening at ${this.port} port.`);
+    });
   }
 
 }
 
-new App()
+new App();

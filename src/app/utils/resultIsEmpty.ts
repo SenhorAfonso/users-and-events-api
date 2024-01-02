cbb405d7056ec9a618897c9d43516fe33dd6a7fa
Pedro Afonso <sirafonso005896@outlook.com>
2024-01-02 17:56:06 -0300

    :recycle: refactor: APIUtils functions moved to APIUtils class

--- src/app/utils/resultIsEmpty.ts
+++
@@ -1,9 +0,0 @@
-const resultIsEmpty = (target: any[] | any) => {
-  if (Array.isArray(target)) {
-    return target.length === 0;
-  } else {
-    return target === null || target === undefined;
-  }
-}
-
-export default resultIsEmpty;
